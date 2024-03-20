cda1f77238d3f7e7a591995f2d3cb9a6c4ac876e
Waqar Ahmad <waqar4704649@gmail.com>
2024-03-20 15:49:12 +0500

    chnage the color of rating icon

--- src/utilities/StarRating.jsx
+++ src/utilities/StarRating.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 const StarRating = ({ rating }) => {
-  const MAX_RATING = 5; // Maximum rating
-  const filledStars = Math.floor(rating); // Number of filled stars
-  const remainder = rating - filledStars; // Remaining fraction for half star
-  const emptyStars = MAX_RATING - filledStars - (remainder >= 0.5 ? 1 : 0); // Number of empty stars
+  const MAX_RATING = 5;
+  const filledStars = Math.floor(rating);
+  const remainder = rating - filledStars;
+  const emptyStars = MAX_RATING - filledStars - (remainder >= 0.5 ? 1 : 0);
 
-  const renderFilledStar = () => <span className="text-2xl">&#9733;</span>;
+  const renderFilledStar = () => (
+    <span className="text-2xl text-yellow-300">&#9733;</span>
+  );
 
-  const renderEmptyStar = () => <span className="text-2xl">&#9734;</span>; // Unicode character for empty star
+  const renderEmptyStar = () => (
+    <span className="text-2xl text-yellow-400">&#9734;</span>
+  );
 
   return (
     <div>
